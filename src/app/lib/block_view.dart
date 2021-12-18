04214460cbab0e341f3a64e8668952af1d7f6007
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-12-18 19:05:47 +0100

    Added NumberProvider, TextProvider, CappedValueProvider

--- src/app/lib/block_view.dart
+++ src/app/lib/block_view.dart
@@ -38,6 +38,67 @@ abstract class InputProvider<T> extends StatelessWidget {
 
   final void Function(T result) onSubmit;
 }
+
+class TextProvider extends InputProvider<String> {
+  const TextProvider(
+      {required void Function(String) onSubmit, this.hintText, Key? key})
+      : super(onSubmit: onSubmit, key: key);
+
+  final String? hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+        maxLines: 1,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: hintText ?? 'Here goes your Text',
+        ),
+        onSubmitted: (String text) => onSubmit(text));
+  }
+}
+
+class NumberProvider extends InputProvider<int> {
+  const NumberProvider({required void Function(int) onSubmit, Key? key})
+      : super(onSubmit: onSubmit, key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+        keyboardType: TextInputType.number,
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.digitsOnly
+        ],
+        maxLines: 1,
+        decoration: const InputDecoration(
+          border: InputBorder.none,
+        ),
+        onSubmitted: (String text) => onSubmit(int.parse(text)));
+  }
+}
+
+class CappedValueProvider extends InputProvider<String> {
+  const CappedValueProvider(
+      {required this.possibleValues,
+      required void Function(String) onSubmit,
+      Key? key})
+      : super(onSubmit: onSubmit, key: key);
+
+  final List<String> possibleValues;
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<String>(
+      tooltip: 'Select one of ${possibleValues.length} Values',
+      onSelected: (String result) => onSubmit(result),
+      itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+        for (String value in possibleValues)
+          PopupMenuItem<String>(child: Text(value), value: value)
+      ],
+    );
+  }
+}
+
 abstract class Block extends StatefulWidget {
   const Block({Key? key}) : super(key: key);
 
