b24ed958f009493c67e9fd925371cb21e4f189a7
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-12-18 19:08:01 +0100

    Added very basic settings view

---
+++ src/app/lib/settings_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:robot_router/main.dart';
+
+class SettingsView extends StatefulWidget {
+  const SettingsView({Key? key}) : super(key: key);
+
+  @override
+  _SettingsViewState createState() => _SettingsViewState();
+}
+
+class _SettingsViewState extends State<SettingsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Setting')), body: const Text(''));
+  }
+}
