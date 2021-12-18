78e5051d5c81f9a1f07e3714b2d00d76bbfc03eb
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-12-18 19:07:02 +0100

    Added a block with a subblock

--- src/app/lib/block_view.dart
+++ src/app/lib/block_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:robot_router/custom_blocks/block_with_subblock.dart';
 import 'package:robot_router/custom_blocks/comment_block.dart';
 import 'package:robot_router/custom_blocks/test_block.dart';
+
 enum BlockType { t_action, t_passive, t_event, t_control }
 
 Future<Block?> selectBlockFromList(BuildContext context) {
@@ -119,6 +121,7 @@ abstract class Block extends StatefulWidget {
     final List<Block> blocks = <Block>[
       const TestBlock(),
       const CommentBlock(),
+      const BlockWithSubblock(),
     ];
     List<List<Block>> result = List<List<Block>>.generate(
         BlockType.values.length, (int index) => <Block>[]);
---
+++ src/app/lib/custom_blocks/block_with_subblock.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:robot_router/block_view.dart';
+
+class BlockWithSubblock extends Block {
+  const BlockWithSubblock({Key? key}) : super(key: key);
+
+  @override
+  BlockState createState() => BlockWithSubblockState();
+
+  @override
+  String get name => 'Block with subblock';
+
+  @override
+  BlockType get type => BlockType.t_action;
+}
+
+class BlockWithSubblockState extends BlockState {
+  Block? _subblock;
+
+  @override
+  Widget render() {
+    return Row(
+      children: <Widget>[
+        const Text('This is a Block:'),
+        Expanded(
+          child: InkWell(
+              child: Card(
+                  color: Colors.grey,
+                  shape: ContinuousRectangleBorder(
+                    side: const BorderSide(color: Colors.black45, width: 1),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: _subblock ??
+                        const Text('Click the grey area to select a block',
+                            style: TextStyle(color: Colors.white)),
+                  )),
+              onTap: () {
+                selectBlockFromList(context).then((Block? block) => {
+                      setState(() {
+                        _subblock = block ?? _subblock;
+                      })
+                    });
+              }),
+        ),
+      ],
+    );
+  }
+}
