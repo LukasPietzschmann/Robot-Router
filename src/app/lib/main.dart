3b9025e56fea9e9c933eb20cd175068da6a959c8
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2022-01-24 14:26:34 +0100

    Don't show debug banner

--- src/app/lib/main.dart
+++ src/app/lib/main.dart
@@ -14,6 +14,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Robot Router',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
